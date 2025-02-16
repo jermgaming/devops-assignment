e1b12cf014d99e7c7e05aee52c20381ea1664f03
jermgaming <atinder999@gmail.com>
2025-02-17 00:00:54 +0530

    fixed go-lang code and made changes to docker file

--- docker-go/main.go
+++ docker-go/main.go
@@ -12,8 +12,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	counter++
 	fmt.Fprintf(w, "You are visitor number %d\n", counter)
 
-	subPath := r.URL.Path[1:2]
-	fmt.Fprintf(w, "First character of path is: %s\n", subPath)
+	if len(r.URL.Path) > 1 {
+		subPath := r.URL.Path[1:2]
+		fmt.Fprintf(w, "First character of path is: %s\n", subPath)
+	} else {
+		fmt.Fprintf(w, "Path is too short to extract first character\n")
+	}
 }
 
 func main() {
